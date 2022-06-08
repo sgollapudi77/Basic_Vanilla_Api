b5e37a85d132076982c8289bce912a1b72146fde
sgollapudi77 <85578033+sgollapudi77@users.noreply.github.com>
2022-06-08 16:49:19 +0530

    Working API response

--- api/HttpTrigger2/index.js
+++ api/HttpTrigger2/index.js
@@ -8,6 +8,10 @@ module.exports = async function (context, req) {
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body:
+        {
+            text: responseMessage,
+        },
+        contentType: 'application/json'
     };
 }
